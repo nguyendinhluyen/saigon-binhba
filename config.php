14f84d48800cc2821b90d061d145592bb18820d2
Nguyen Dinh Luyen <luyen.nguyen.itbb@jvn.edu.vn>
2017-09-30 19:24:58 +0700

    update htaccess and config local

--- config.php
+++ config.php
@@ -1,10 +1,17 @@
 <?php
 class config {
     var $dbtype = 'mysql';
-    var $host = 'localhost';
-    var $user = 'saigonbi_user';
-    var $password = 'so2XuXgGTc2B';
+//    var $host = 'localhost';
+//    var $user = 'saigonbi_user';
+//    var $password = 'so2XuXgGTc2B';
+//    var $db = 'saigonbi_db';
+//    var $display_error =0;
+    
+    var $host = '127.0.0.1:3306';
+    var $user = 'root';
+    var $password = '';
     var $db = 'saigonbi_db';
     var $display_error =0;
+
 }
 ?>
